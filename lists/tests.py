2af3c9aa9c627af2810b5912667d679fbf921cc6
kimjdo <kimjdo@naver.com>
2018-04-10 14:48:27 +0900

    Refactor Home_page use templates

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from .views import home_page
 
@@ -11,9 +12,16 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_homepage_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>일정관리</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+#        request = HttpRequest()
+#        response = home_page(request)
+
+        response = self.client.get('/')
+
+#        html = response.content.decode('utf8')
+#        expected_html = render_to_string('home.html')
+#        self.assertEqual(html, expected_html)
+#        self.assertTrue(html.startswith('<html>'))
+#        self.assertIn('<title>일정관리</title>', html)
+#        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
