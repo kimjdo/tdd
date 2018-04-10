8065bd708a93f3a28dfb11249d91e358a59a3e33
kimjdo <kimjdo@naver.com>
2018-04-10 15:06:12 +0900

    HTML code complete

--- function_tests.py
+++ function_tests.py
@@ -18,8 +18,8 @@ class NewVisitorTest(unittest.TestCase):
 
         # 홈페이지에 방문해 보니 제목이 '일정관리'인 것을 보고 홈페이지에 방문한 것을 확인한다.
         self.assertIn('일정관리', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('hi').text
-        self.assertIn('일정목록', header_text)\
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('일정목록', header_text)
 
         # 일정을 입력할 수 있는 페이지로 바로 이동한다.
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -30,12 +30,14 @@ class NewVisitorTest(unittest.TestCase):
 
         # 엔터를 입력하면 페이지를 새로고침해서 모든 일정 목록을 보여준다.
         # "1: 시장에서 미역사기"가 첫 번째 할 일로 일정 목록에서 보여진다.
-        inputbox.send_keys(not keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(nay(row.text == '1: 시장에서 미역사기' for row in rows))
+        self.assertTrue(any(row.text == '1: 시장에서 미역사기' for row in rows),
+                        "New to-do item did not appear in table"
+                        )
         # 사용자는 추가로 할 일 텍스트박스에 입력할 수 있고
         # '미역을 물에 불리기'라고 입력한다.
         self.fail("테스트 종료")
