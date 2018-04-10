2cb5ac3d6943c138697002ec78cc71dfd3fd2ecc
kimjdo <kimjdo@naver.com>
2018-04-10 14:30:20 +0900

    Functional test now checks we can input a to-do tiem

--- function_tests.py
+++ function_tests.py
@@ -1,5 +1,8 @@
+import time
 import unittest
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -15,15 +18,29 @@ class NewVisitorTest(unittest.TestCase):
 
         # 홈페이지에 방문해 보니 제목이 '일정관리'인 것을 보고 홈페이지에 방문한 것을 확인한다.
         self.assertIn('일정관리', self.browser.title)
-        self.fail("테스트 종료")
+        header_text = self.browser.find_element_by_tag_name('hi').text
+        self.assertIn('일정목록', header_text)\
+
         # 일정을 입력할 수 있는 페이지로 바로 이동한다.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), '할 일을 입력하세요.')
 
         # 생일날 미역국을 끓이기 위해 텍스트 박스에 '시장에서 미역사기'를 입력한다.
+        inputbox.send_keys('시장에서 미역사기')
 
         # 엔터를 입력하면 페이지를 새로고침해서 모든 일정 목록을 보여준다.
+        # "1: 시장에서 미역사기"가 첫 번째 할 일로 일정 목록에서 보여진다.
+        inputbox.send_keys(not keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(nay(row.text == '1: 시장에서 미역사기' for row in rows))
         # 사용자는 추가로 할 일 텍스트박스에 입력할 수 있고
         # '미역을 물에 불리기'라고 입력한다.
+        self.fail("테스트 종료")
+
+
 
         # 사용자는 일정 목록이 사이트에 올바르게 저장되었는지 궁금해서 고유 URL 생성을 확인한다.
 
--- lists/views.py
+++ lists/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page(request):
-    return HttpResponse('<html><title>일정관리</title></html>')
+    return HttpResponse('<html><title>일정관리</title><body><h1>hi</h1></body></html>')
